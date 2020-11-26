516476f2f90863c56f7a81f8fc130e7b0dbdda33
Neagu Mihai <myhay199961@gmail.com>
2020-11-26 14:46:19 +0200

    broken build

---
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package guru.sfg.brewery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests(auth -> {
+            auth.antMatchers("/", "/webjars/**", "/login").permitAll();
+        })
+        .authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin().and()
+        .httpBasic();
+    }
+}
